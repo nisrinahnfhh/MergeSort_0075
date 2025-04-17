28ee0468ab764b1896809e7f607adab3440d98ab
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-04-17 18:23:52 +0700

    step 2

--- mergesort.cpp
+++ mergesort.cpp
@@ -2,27 +2,14 @@
 using namespace std;
 
 int arr[20], B[20]; // Array utama dan array bantu
-int n;
+int n; // Jumlah elemen dalam Array
 
-
-void mergeSort(int low, int high)
-{
-
-    if (low >= high)
-    { // step 1
+void mergeSort(int low, int high) {
+ if (low >= high) { // step 1
         return; // step 1a}
 }
 
+int mid = (low + high) / 2; // step 2
 
-void input(){
-    while(true){
-        cout << "Masukkan Panjang Element Array: ";
-        cin >> n;
-        if(n<= 20){
-            break;
-        } else {
-            cout
-        }
-    }
 
-}
+    
