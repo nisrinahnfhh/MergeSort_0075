e89606ad2bd228069613e570092c142de3ff8141
nisrinahnfhh <nisrinahnfhh@gmail.com>
2025-04-17 18:18:17 +0700

    step 1

--- mergesort.cpp
+++ mergesort.cpp
@@ -5,9 +5,24 @@ int arr[20], B[20]; // Array utama dan array bantu
 int n;
 
 
+void mergeSort(int low, int high)
+{
+
+    if (low >= high)
+    { // step 1
+        return; // step 1a}
+}
+
+
 void input(){
     while(true){
         cout << "Masukkan Panjang Element Array: ";
-        
+        cin >> n;
+        if(n<= 20){
+            break;
+        } else {
+            cout
+        }
     }
+
 }
